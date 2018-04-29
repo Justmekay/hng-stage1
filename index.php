e0c6760baced9e3926df992b183a5db5f96bd884
Ogunwole <corlade94@gmail.com>
2018-04-29 17:35:55 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HNG Internship</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<div class="main">
+		<h2 class="welcome">Welcome To</h2>
+		<h1 class="intern">HNG <br> INTERNSHIP <br>PAGE</h1>
+		<?php
+			$date = date("d M, Y h:i A");
+			print('<p class="date">'.$date.'</p>');
+		?>
+		
+	</div>
+
+</body>
+</html>
